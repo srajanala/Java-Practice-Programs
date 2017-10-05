5bd873d95ef10384e4d50cf8bdb4fe23dc2cf033
srajanala <srajanala@users.noreply.github.com>
2017-10-04 21:25:32 -0500

    Add files via upload
    Assertion programs

---
+++ AssertionExample.java
@@ -0,0 +1,12 @@
+package Assertionspackage;
+import java.util.Scanner;
+class AssertionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int value =15;
+         assert value >=20 : "Underweight";
+          System.out.println("value is "+value);
+	}
+
+}
