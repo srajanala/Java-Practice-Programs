84df09b27e884cf530372f109654399ecc94d6b2
srajanala <srajanala@users.noreply.github.com>
2017-10-10 12:15:19 -0500

    Add files via upload
    generics example

--- Example1.java
+++ Example1.java
@@ -1,23 +1,27 @@
-package BaseAndDerivedCatch;
+package Generics;
 
-class Base extends Exception{}
-
-class Derived extends Base{}
+class Test<T>{
+	T obj;
+	
+    Test(T obj){
+    	this.obj=obj;
+    }
+    
+    public T getObject(){
+    	return this.obj;
+    }
+    
+}
 
 public class Example1 {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      try{
-    	  throw new Derived();
-      }catch(Derived d){
-    	  System.out.println("Derived Exception is caught");
-      }
-      catch(Base b){
-    	  System.out.println("Base Exception is caught");
-      }
-      
-     
+              Test<Integer> iobj=new Test<Integer>(15);
+              Test<String> sobj = new Test<String>("GeekforGeeks");
+              System.out.println(iobj.getObject());
+              System.out.println(sobj.getObject());
 	}
 
 }
+
