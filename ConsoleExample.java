ca43cb5ee07cd51d176fc079240ee15ac0becfaa
srajanala <srajanala@users.noreply.github.com>
2017-10-11 22:29:34 -0500

    Add files via upload

---
+++ ConsoleExample.java
@@ -0,0 +1,11 @@
+package InputOutputPackage;
+
+public class ConsoleExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String name=System.console().readLine();
+        System.out.println(name);
+	}
+
+}
