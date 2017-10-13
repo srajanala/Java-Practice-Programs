eb7d69177ba2a6971eeab17d3a61b2745f8c8733
srajanala <srajanala@users.noreply.github.com>
2017-10-13 16:06:54 -0500

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+package Interface;
+
+class test implements in1{
+	 public void display(){
+		 System.out.println("implementing interface");
+	 }
+	 public static void main(String[] args){
+		 test t=new test();
+		 t.display();
+		 System.out.println(a);
+	 }
+}
