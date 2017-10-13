56879568232e4dcc5ff0301e8c651c75dbe9d576
srajanala <srajanala@users.noreply.github.com>
2017-10-12 19:19:28 -0500

    Add files via upload
    Interface Example

---
+++ Bicycle.java
@@ -0,0 +1,19 @@
+package Interface;
+
+
+class Bicycle implements Vehicle{
+	int speed;
+	int gear;
+	public void changeGear(int newgear){
+		gear=newgear;
+	}
+    public void speedUp(int increment){
+		speed=speed+increment;
+	}
+    public void applyBreaks(int decrement){
+		speed=speed-decrement;
+	}
+    public void printstate(){
+    	System.out.println("speed: "+ speed+" gear: "+gear);
+    }
+}
