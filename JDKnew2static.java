c6fa739f657f57e198e1ef0e9f7e763dd09930a4
srajanala <srajanala@users.noreply.github.com>
2017-10-13 16:06:16 -0500

    Add files via upload

---
+++ JDKnew2static.java
@@ -0,0 +1,10 @@
+package Interface;
+
+public class JDKnew2static implements InterfaceJDKnew2{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InterfaceJDKnew2.display();
+	}
+
+}
