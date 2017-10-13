bd9dde3b9b97df126c917fa533f62736534c1775
srajanala <srajanala@users.noreply.github.com>
2017-10-12 19:22:21 -0500

    Add files via upload

---
+++ InterfaceJDKnew2.java
@@ -0,0 +1,7 @@
+package Interface;
+
+public interface InterfaceJDKnew2 {
+static void display(){
+	System.out.println("new feature static methods in interface can be called without any object");
+}
+}
