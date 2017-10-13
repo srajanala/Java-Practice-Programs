1c2095206343b7357855c1f324ea6eab59909615
srajanala <srajanala@users.noreply.github.com>
2017-10-13 16:05:59 -0500

    Add files via upload

---
+++ JDKnew1.java
@@ -0,0 +1,11 @@
+package Interface;
+
+public class JDKnew1 implements InterfaceJDKnew1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JDKnew1 j1 =new JDKnew1();
+		j1.display();
+	}
+
+}
