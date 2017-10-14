7918a578124730f73929836867abf41277503ca5
srajanala <srajanala@users.noreply.github.com>
2017-10-14 15:24:35 -0500

    Add files via upload

---
+++ EnumerationExample.java
@@ -0,0 +1,18 @@
+package Iterators_Collection_Package;
+import java.util.*;
+public class EnumerationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Vector v = new Vector();
+       for(int i=0;i<10;i++) 
+          v.addElement(i);
+          System.out.println(v);
+       Enumeration e = v.elements();
+       while(e.hasMoreElements()){
+    	   int i = (Integer)e.nextElement();
+    	  System.out.print(i+" "); 
+       }
+	}
+
+}
