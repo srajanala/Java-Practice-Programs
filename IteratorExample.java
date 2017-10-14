f6fd46b3a9c2f26e07874e76e817ff414490cc25
srajanala <srajanala@users.noreply.github.com>
2017-10-14 15:24:55 -0500

    Add files via upload

---
+++ IteratorExample.java
@@ -0,0 +1,25 @@
+package Iterators_Collection_Package;
+import java.util.*;
+
+public class IteratorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       ArrayList al = new ArrayList();
+       for(int i=0;i<10;i++)
+       al.add(i);
+       System.out.println(al);
+       
+       Iterator itr = al.iterator();
+       while(itr.hasNext()){
+    	   int i =(Integer)itr.next();
+    	   System.out.print(i+" ");
+    	   
+    	   if(i%2!=0)
+    		   itr.remove();
+    	   }
+       System.out.println();
+       System.out.println(al);
+	}
+
+}
