c5f523b64bd91cad9cb4e5e012cda116acb1ac2b
Sai Sindhu <srajanala@users.noreply.github.com>
2017-10-16 13:23:07 -0500

    Add files via upload
    Java Interface Methods

--- A.java
+++ A.java
@@ -1,27 +1,10 @@
-package ComparatorInterface;
-import java.util.Comparator;
+package JavaInterfaceMethods;
 
-
-public class A{
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-  B b =new B();
-  System.out.println(b.compare(new Integer(10), new Integer(20)));
-	}
-
+public interface A {
+void fun();
 }
-
-
-class B implements Comparator{
-public int compare(Object obj1, Object obj2){
-	
-	Integer i1=(Integer)obj1;
-	Integer i2=(Integer)obj2;
-	if(i1>i2){return 1;}
-	else if(i1==i2){return 0;}
-	else {return -1;}
-	
-	
-}
-	
-}
+ class B implements A{
+	 public void fun(){
+		 System.out.println("fun is created");
+	 }
+ }
---
+++ C.java
@@ -0,0 +1,11 @@
+package JavaInterfaceMethods;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       B b =new B();
+       b.fun();
+	}
+
+}
