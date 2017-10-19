ef7f3e4efc4483f04ef1ce1586f057bd61834378
Sai Sindhu <srajanala@users.noreply.github.com>
2017-10-19 17:10:40 -0500

    Add files via upload
    Lambda expression example

--- Test.java
+++ Test.java
@@ -1,44 +1,14 @@
-package ImmutableClass;
+package LambdaExpressionPackage;
 
-final class Student {
-	
-	{
-		System.out.println("Initialization Block");
-	}
-	final String name;
-	final int regno;
-	public Student(String name, int regno){
-		System.out.println("Constructor");
-		this.name=name;
-		this.regno=regno;
-	}
-	public String getName(){
-		return name;
-	}
-	public int getRegno(){
-		return regno;
-	}
-	
+interface FuncInterface{
+	void abstractfun();
 }
-
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       Student s = new Student("sai",2020);
-       System.out.println("name: "+s.name);
-       System.out.println("regno: "+s.regno);
-       
-       
-       
-       String s2 = "hello";
-       
-       String s1 = new String("hello");
-       String s3 = "hello";
-       System.out.println(s1 == s2);
-       System.out.println(s1.equals(s2));
-       
-    		 
+      FuncInterface fobj = ()->System.out.println("Hello");
+      fobj.abstractfun();
 	}
 
 }
