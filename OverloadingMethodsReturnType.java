9ffb4c3983787a400be04444489af7b576bd6435
Sai Sindhu <srajanala@users.noreply.github.com>
2017-10-21 21:19:06 -0500

    Add files via upload

---
+++ OverloadingMethodsReturnType.java
@@ -0,0 +1,11 @@
+package MethodOverloading;
+
+public class OverloadingMethodsReturnType {
+	public int foo(){return 10;}
+    public char foo(){return 20}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+	}
+
+}
