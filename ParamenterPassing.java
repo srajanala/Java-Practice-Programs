f97add42a0e38f86247c143f8719bdc7ec96c4f5
Sai Sindhu <srajanala@users.noreply.github.com>
2017-10-23 21:30:30 -0500

    Add files via upload

---
+++ ParamenterPassing.java
@@ -0,0 +1,18 @@
+package MethodsInJava;
+
+public class ParamenterPassing {
+	public static void swap(Integer i, Integer j){
+		Integer temp = new Integer(i);
+		i=j;
+		j=temp;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Integer i = new Integer(10);
+          Integer j =new Integer(20);
+          swap(i,j);
+          System.out.println("i = "+i+" j = "+j);
+	}
+
+}
