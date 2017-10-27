c394f547b8cc291c3ed475c420f636c272668d8f
Sai Sindhu <srajanala@users.noreply.github.com>
2017-10-27 15:57:06 -0500

    Add files via upload

---
+++ OI.java
@@ -0,0 +1,13 @@
+package NestedInterface;
+
+public class OI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      OutInterface.InnInterface obj;
+      Testingg tt=new Testingg();
+      obj=tt;
+      obj.show();
+	}
+
+}
