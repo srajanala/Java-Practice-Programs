4d31dfa45481f97f17d3e849bc52a88391f253db
Sai Sindhu <srajanala@users.noreply.github.com>
2017-10-27 15:56:41 -0500

    Add files via upload

---
+++ ToString.java
@@ -0,0 +1,15 @@
+package Object;
+import java.util.ArrayList;
+public class ToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Integer i = new Integer(50);
+         ArrayList list =new ArrayList();
+         list.add(50);
+         list.add("Hello world");
+         System.out.println(i.toString());
+         System.out.println(list.toString());
+	}
+
+}
