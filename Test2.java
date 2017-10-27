1d4ee4029bc44123d4ccd34cc2d3c42d445d1e69
Sai Sindhu <srajanala@users.noreply.github.com>
2017-10-27 15:57:51 -0500

    Add files via upload

--- Test2.java
+++ Test2.java
@@ -1,20 +1,23 @@
-package LambdaExpressionPackage;
-import java.util.ArrayList;
-
+package NestedInterface;
+class ProtectedInter{
+	interface yes{
+		void show();
+	}
+}
+class Pp implements ProtectedInter.yes{
+	public void show(){
+		System.out.println("nested interface");
+	}
+}
 public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        a.add(1);
-        a.add(2);
-        a.add(3);
-        a.add(4);
-        a.add(5);
-        a.add(6);
-        
-       a.forEach(n->System.out.println(n));
-       a.forEach(n->{if(n%2==0) System.out.println(n);});
+       ProtectedInter.yes obj;
+       Pp p=new Pp();
+       obj=p;
+       obj.show();
+       
 	}
 
 }
